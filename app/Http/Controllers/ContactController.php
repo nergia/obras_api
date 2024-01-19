c09fc23fcb191dc76b165ee862f4baa7c4a59ae3
nergia <matheussr000@gmail.com>
2024-01-19 16:28:43 -0300

    function Create Contact

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -16,4 +16,13 @@ class ContactController extends Controller
                 ]
             );
     }
+    public function saveContact(Request $request){
+        $contact = new Contact();
+        $contact->name = $request->name;
+        $contact->email = $request->email;
+        $contact->designation = $request->designation;
+        $contact->contact_no = $request->contact_no;
+        $contact->save();
+        return response()->json(['message'=>'Contact Created Successfully', 'code'=> 200] );
+    }
 }
